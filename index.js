78f4ebb549bc2cc11321acef5a3c1cbda516d765
Martin Heidegger <martin.heidegger@gmail.com>
2020-07-06 23:14:50 +0900

    refactor: extracting processFeed functionality for easier tests

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const fs        = require('fs');
-const chalk     = require('chalk');
-const fetchData = require('./fetchData');
+const chalk       = require('chalk');
+const processFeed = require('./processFeed');
 
 module.exports = {
 
@@ -8,24 +7,10 @@ module.exports = {
 
     // Gather the data from all the specified feeds
     for (const feed of inputs.feeds) {
-
-      // Where fetched data should reside in the buid
-      let dataFilePath = `${inputs.dataDir}/${feed.name}.json`;
-
-      // reinstate from cache if it is present
-      if ( await utils.cache.has(dataFilePath) ) {
-        await utils.cache.restore(dataFilePath);
+      if (await processFeed(utils.cache, inputs.dataDir, feed)) {
         console.log('Restored from cache:', chalk.green(feed.url));
-      }
-      // Or if it's not cached, let's fetch it and cache it.
-      else {
-        const data = await fetchData(feed);
-
-        // put the fetched data in the daa file, and then cache it.
-        await fs.writeFileSync(dataFilePath, data);
-        await utils.cache.save(dataFilePath, { ttl: feed.ttl });
+      } else {
         console.log('Fetched and cached: ', chalk.yellow(feed.url), chalk.gray(`(TTL:${feed.ttl} seconds)`));
-
       }
     }
   }
